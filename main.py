9e26822684d7c49b84bc4a89713e612998ee0fba
Rishi Advani <rishi1999@gmail.com>
2019-03-04 17:31:23 -0500

    increased animation smoothness by utilizing pygame Clock object; abstracted out more functions/data

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ import time
 pygame.init()
 
 # display constants
-SIZE = WIDTH, HEIGHT = 1000, 600
+SIZE = (WIDTH, HEIGHT) = (1000, 600)
 BLACK = 0, 0, 0
 RED = 255, 0, 0
 
 # physics engine constants
-PERIOD = 0.018
 METER = 250.0
 GRAVITY = 9.8
-
-# TODO somehow starting at (100, 100) causes the ball to gain momentum?? pls fix.
 
 
 def main():
     screen = pygame.display.set_mode(SIZE)
+    global screen
 
     b = Ball()
+    clock = pygame.time.Clock()
 
     while True:
         for event in pygame.event.get():
@@ -33,14 +32,16 @@ def main():
             if event.type == pygame.MOUSEBUTTONUP:
                 b.held = False
 
-        old_rect = b.rect
+        old_rect = b.rect.copy()
+
+        interval = clock.tick(60) / 1000.0
 
         if b.held:
-            b.drag()
+            b.drag(interval)
         else:
-            b.move(old_rect)
+            b.move(old_rect, interval)
 
-        b.update_graphics(screen, old_rect)
+        b.update_graphics(old_rect)
 
 
 class Ball:
@@ -51,55 +52,45 @@ class Ball:
         pygame.draw.circle(self.shadow, BLACK, (r, r), r, 0)
         self.speed = [2.0, 0.0]
         self.rect = self.image.get_rect(center=center_location)
+        self.r = r
         self.floored = False
         self.held = False
 
-    def move(self, old_rect):
+    def move(self, old_rect, TIME):
         # prevent ball from "jittering" when at bottom of screen
         if abs(self.speed[1]) < 0.05 and self.rect.bottom > HEIGHT - 5:
             self.speed[1] = 0
-            self.rect.bottom = HEIGHT
+            self.rect.bottom = float(HEIGHT)
             self.floored = True
 
         # apply gravity to ball
         if not self.floored:
-            self.speed[1] += GRAVITY * PERIOD
+            self.speed[1] += GRAVITY * TIME
 
         # apply air resistance to ball
         absolute_speed = math.sqrt(self.speed[0] ** 2 + self.speed[1] ** 2)
-        self.speed[0] -= 0.1 * PERIOD * self.speed[0] / absolute_speed
-        self.speed[1] -= 0.1 * PERIOD * self.speed[1] / absolute_speed
+        self.speed[0] -= 0.1 * TIME * self.speed[0] / absolute_speed
+        self.speed[1] -= 0.1 * TIME * self.speed[1] / absolute_speed
 
         # update ball location
-        self.rect.left += (self.speed[0] * PERIOD * METER)
-        self.rect.top += (self.speed[1] * PERIOD * METER)
-        delta_x = int(self.rect.left - old_rect.left)
-        delta_y = int(self.rect.top - old_rect.top)
-        self.rect = self.rect.move(delta_x, delta_y)
+        self.rect.move_ip([x * TIME * METER for x in self.speed])
 
         # deal with collisions
-        if self.rect.left < 0:
-            self.rect.left = 0
+        if self.rect.left < 0 or self.rect.right > WIDTH:
             self.speed[0] = -self.speed[0]
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-            self.speed[0] = -self.speed[0]
-        if self.rect.top < 0:
-            self.rect.top = 0
+        if self.rect.top < 0 or self.rect.bottom > HEIGHT:
             self.speed[1] = -self.speed[1]
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
-            self.speed[1] = -self.speed[1]
+        self.rect.clamp(screen.get_rect())
 
-    def drag(self):
-        coords = pygame.mouse.get_pos()
+    def drag(self, TIME):
+        coords = [x - self.r for x in pygame.mouse.get_pos()]
         self.rect.left = coords[0]
         self.rect.top = coords[1]
         delta = pygame.mouse.get_rel()
-        self.speed = [x / 10 for x in delta]
-        self.rect = self.rect.move(delta)
+        self.speed = [x / METER / TIME for x in delta]
+        self.rect.move_ip(delta)
 
-    def update_graphics(self, screen, old_rect):
+    def update_graphics(self, old_rect):
         # clear old ball image from screen
         screen.blit(self.shadow, old_rect)
 
@@ -107,7 +98,6 @@ class Ball:
         screen.blit(self.image, self.rect)
 
         pygame.display.update()
-        time.sleep(PERIOD)
 
 
 if __name__ == '__main__':
