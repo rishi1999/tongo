5b076f9cd5934ec1c882914d52e46d5efac4af7f
Rishi Advani <rishi1999@gmail.com>
2019-03-04 17:56:40 -0500

    fixed a bunch of division by 0, clamping errors; removed effects of air resistance when ball is floored (TODO will implement friction); refactored code to remove unnecessary field  from Ball definition

--- main.py
+++ main.py
@@ -32,16 +32,16 @@ def main():
             if event.type == pygame.MOUSEBUTTONUP:
                 b.held = False
 
-        old_rect = b.rect.copy()
+        b.old_rect = b.rect.copy()
 
         interval = clock.tick(60) / 1000.0
 
         if b.held:
             b.drag(interval)
         else:
-            b.move(old_rect, interval)
+            b.move(interval)
 
-        b.update_graphics(old_rect)
+        b.update_graphics()
 
 
 class Ball:
@@ -52,35 +52,36 @@ class Ball:
         pygame.draw.circle(self.shadow, BLACK, (r, r), r, 0)
         self.speed = [2.0, 0.0]
         self.rect = self.image.get_rect(center=center_location)
+        self.old_rect = None
         self.r = r
-        self.floored = False
         self.held = False
 
-    def move(self, old_rect, TIME):
+    def move(self, TIME):
         # prevent ball from "jittering" when at bottom of screen
-        if abs(self.speed[1]) < 0.05 and self.rect.bottom > HEIGHT - 5:
+        if abs(self.speed[1]) < 0.05 and self.rect.bottom == HEIGHT:
             self.speed[1] = 0
-            self.rect.bottom = float(HEIGHT)
-            self.floored = True
-
-        # apply gravity to ball
-        if not self.floored:
+        else:
+            # apply gravity to ball
             self.speed[1] += GRAVITY * TIME
 
-        # apply air resistance to ball
-        absolute_speed = math.sqrt(self.speed[0] ** 2 + self.speed[1] ** 2)
-        self.speed[0] -= 0.1 * TIME * self.speed[0] / absolute_speed
-        self.speed[1] -= 0.1 * TIME * self.speed[1] / absolute_speed
+            # apply air resistance to ball
+            absolute_speed = math.sqrt(self.speed[0] ** 2 + self.speed[1] ** 2)
+            self.speed[0] -= 0.1 * TIME * self.speed[0] / absolute_speed
+            self.speed[1] -= 0.1 * TIME * self.speed[1] / absolute_speed
 
-        # update ball location
+            # TODO add friction
+
+        # update ball location and velocity
         self.rect.move_ip([x * TIME * METER for x in self.speed])
-
-        # deal with collisions
-        if self.rect.left < 0 or self.rect.right > WIDTH:
-            self.speed[0] = -self.speed[0]
-        if self.rect.top < 0 or self.rect.bottom > HEIGHT:
-            self.speed[1] = -self.speed[1]
-        self.rect.clamp(screen.get_rect())
+        self.rect.clamp_ip(screen.get_rect())
+        if self.rect.left == 0:
+            self.speed[0] = abs(self.speed[0])
+        if self.rect.right == WIDTH:
+            self.speed[0] = -abs(self.speed[0])
+        if self.rect.top == 0:
+            self.speed[1] = abs(self.speed[1])
+        if self.rect.bottom == HEIGHT:
+            self.speed[1] = -abs(self.speed[1])
 
     def drag(self, TIME):
         coords = [x - self.r for x in pygame.mouse.get_pos()]
@@ -90,9 +91,9 @@ class Ball:
         self.speed = [x / METER / TIME for x in delta]
         self.rect.move_ip(delta)
 
-    def update_graphics(self, old_rect):
+    def update_graphics(self):
         # clear old ball image from screen
-        screen.blit(self.shadow, old_rect)
+        screen.blit(self.shadow, self.old_rect)
 
         # draw current ball image on screen
         screen.blit(self.image, self.rect)
