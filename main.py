45745121d2215c856957e0bc62ddc0a6cf1d9d70
Rishi Advani <rishi1999@gmail.com>
2019-03-05 00:03:04 -0500

    improved physics engine
    added coefficient of friction, restitution. ball bouncing is now much more realistic.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
-import pygame
 import sys
-import time
+
+import pygame
 
 pygame.init()
 
@@ -13,11 +13,13 @@ RED = 255, 0, 0
 # physics engine constants
 METER = 250.0
 GRAVITY = 9.8
+FRICTION = 0.1
+RESTITUTION = 0.9
 
 
 def main():
-    screen = pygame.display.set_mode(SIZE)
     global screen
+    screen = pygame.display.set_mode(SIZE)
 
     b = Ball()
     clock = pygame.time.Clock()
@@ -37,20 +39,24 @@ def main():
         interval = clock.tick(60) / 1000.0
 
         if b.held:
-            b.drag(interval)
+            b.drag(interval, speed_limit=10.0)
         else:
             b.move(interval)
 
         b.update_graphics()
 
 
+def calculate_speed(velocity):
+    return math.sqrt(velocity[0] ** 2 + velocity[1] ** 2)
+
+
 class Ball:
     def __init__(self, center_location=(WIDTH / 2, HEIGHT / 2), r=50):
-        self.image = pygame.Surface((2*r, 2*r))
+        self.image = pygame.Surface((2 * r, 2 * r))
         pygame.draw.circle(self.image, RED, (r, r), r, 0)
         self.shadow = pygame.Surface((2 * r, 2 * r))
         pygame.draw.circle(self.shadow, BLACK, (r, r), r, 0)
-        self.speed = [2.0, 0.0]
+        self.vel = [2.0, 0.0]
         self.rect = self.image.get_rect(center=center_location)
         self.old_rect = None
         self.r = r
@@ -58,37 +64,54 @@ class Ball:
 
     def move(self, TIME):
         # prevent ball from "jittering" when at bottom of screen
-        if abs(self.speed[1]) < 0.05 and self.rect.bottom == HEIGHT:
-            self.speed[1] = 0
+        if abs(self.vel[1]) < 0.05 and self.rect.bottom == HEIGHT:
+            self.vel[1] = 0
+
+            delta = FRICTION * GRAVITY * TIME
+            if self.vel[0] > 0:
+                self.vel[0] -= delta
+            else:
+                self.vel[0] += delta
         else:
             # apply gravity to ball
-            self.speed[1] += GRAVITY * TIME
+            self.vel[1] += GRAVITY * TIME
 
             # apply air resistance to ball
-            absolute_speed = math.sqrt(self.speed[0] ** 2 + self.speed[1] ** 2)
-            self.speed[0] -= 0.1 * TIME * self.speed[0] / absolute_speed
-            self.speed[1] -= 0.1 * TIME * self.speed[1] / absolute_speed
+            speed = calculate_speed(self.vel)
+            self.vel[0] -= 0.1 * TIME * self.vel[0] / speed
+            self.vel[1] -= 0.1 * TIME * self.vel[1] / speed
 
             # TODO add friction
 
         # update ball location and velocity
-        self.rect.move_ip([x * TIME * METER for x in self.speed])
+        self.rect.move_ip([x * TIME * METER for x in self.vel])
         self.rect.clamp_ip(screen.get_rect())
         if self.rect.left == 0:
-            self.speed[0] = abs(self.speed[0])
+            self.vel[0] = abs(self.vel[0])
+            self.vel[0] *= RESTITUTION
         if self.rect.right == WIDTH:
-            self.speed[0] = -abs(self.speed[0])
+            self.vel[0] = -abs(self.vel[0])
+            self.vel[0] *= RESTITUTION
         if self.rect.top == 0:
-            self.speed[1] = abs(self.speed[1])
+            self.vel[1] = abs(self.vel[1])
+            self.vel[1] *= RESTITUTION
         if self.rect.bottom == HEIGHT:
-            self.speed[1] = -abs(self.speed[1])
+            self.vel[1] = -abs(self.vel[1])
+            self.vel[1] *= RESTITUTION
 
-    def drag(self, TIME):
+    def drag(self, TIME, speed_limit):
         coords = [x - self.r for x in pygame.mouse.get_pos()]
         self.rect.left = coords[0]
         self.rect.top = coords[1]
         delta = pygame.mouse.get_rel()
-        self.speed = [x / METER / TIME for x in delta]
+        self.vel = [x / METER / TIME for x in delta]
+
+        speed = calculate_speed(self.vel)
+        scale = 1
+        if speed > speed_limit:
+            scale = speed_limit / speed
+        self.vel = [x * scale for x in self.vel]
+
         self.rect.move_ip(delta)
 
     def update_graphics(self):
