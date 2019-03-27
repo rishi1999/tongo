c1639cdba61f047ede3bbfb3deeb82434fc45269
Rishi Advani <rishi1999@gmail.com>
2019-03-27 00:03:29 -0400

    modified drag force to be dependent on ball dimensions and speed

--- main.py
+++ main.py
@@ -9,41 +9,45 @@ BG_COLOR = 0, 0, 0
 BALL_COLOR = 255, 0, 0
 
 # physics engine constants
+RADIUS = 0.2
 METER = 250.0
 GRAVITY = 9.8
 FRICTION = 0.1
 POST_EXPLOSION_RESTITUTION = 0.9
 RESTITUTION = 0.9
-
-DEFAULT_RADIUS = 0.2
+AIR_DENSITY = 1.25
+DRAG = 0.75
 
 # TODO add docstrings to functions
 # TODO update readme on github to talk about cli arguments when they are production ready
+# TODO are mass calculations done properly
+# TODO add optional background grid so meter size is clear
 
 
 def hex_to_rgb(color):
     hlen = len(color)
-    return tuple(clamp(int(color[i:i + hlen / 3], 16), max=255) for i in range(0, hlen, hlen / 3))
+    return tuple(clamp(int(color[i:i + hlen / 3], 16), maximum=255) for i in range(0, hlen, hlen / 3))
 
 
-def clamp(val, min=0, max=sys.maxsize):
-    assert min <= max, "clamp: min greater than max"
-    if val < min:
-        val = min
-    elif val > max:
-        val = max
+def clamp(val, minimum=0, maximum=sys.maxsize):
+    assert minimum <= maximum, "clamp: minimum greater than maximum"
+    if val < minimum:
+        val = minimum
+    elif val > maximum:
+        val = maximum
     return val
 
 
 def main(argv):
-    global SIZE, WIDTH, HEIGHT, BG_COLOR, BALL_COLOR, METER, GRAVITY, FRICTION, RESTITUTION
+    global SIZE, WIDTH, HEIGHT, BG_COLOR, BALL_COLOR, RADIUS, METER, GRAVITY, FRICTION, RESTITUTION
     usage_text = """usage: python main.py [options]
         options:
             -h, --help          Displays this usage guide.
-            --width=SIZE        Sets width of the screen to SIZE.
-            --height=SIZE       Sets height of the screen to SIZE.
+            --width=SIZE        Sets width of the screen to SIZE pixels.
+            --height=SIZE       Sets height of the screen to SIZE pixels.
             --bgcolor=COLOR     Sets the background color of the screen to COLOR.
             --ballcolor=COLOR   Sets the color of the ball to COLOR.
+            --radius=NUM        Sets the radius of the ball to NUM meters.
             --meter=NUM         Sets the length of a meter in the simulation to NUM pixels.
             --gravity=NUM       Sets the acceleration due to gravity in the simulation to NUM meters squared per second.
             --friction=NUM      Sets the coefficient of friction in the simulation to NUM.
@@ -51,8 +55,8 @@ def main(argv):
             
             COLOR format examples: 25C3F1, 00FF46, 789ABC, 789abc"""
     try:
-        opts, args = getopt.getopt(argv, "", ["help=", "width=", "height=", "bgcolor=", "ballcolor=", "meter=",
-                                              "gravity=", "friction=", "restitution="])
+        opts, args = getopt.getopt(argv, "", ["help=", "width=", "height=", "bgcolor=", "ballcolor=", "radius=",
+                                              "meter=", "gravity=", "friction=", "restitution="])
     except getopt.GetoptError:
         print usage_text
         sys.exit(2)
@@ -61,10 +65,10 @@ def main(argv):
             print usage_text
             sys.exit()
         elif opt == '--width':
-            WIDTH = clamp(int(arg), 3 * DEFAULT_RADIUS, 4000)
+            WIDTH = clamp(int(arg), 150, 4000)
             SIZE = WIDTH, HEIGHT
         elif opt == '--height':
-            HEIGHT = clamp(int(arg), 3 * DEFAULT_RADIUS, 4000)
+            HEIGHT = clamp(int(arg), 150, 4000)
             SIZE = WIDTH, HEIGHT
         elif opt == '--bgcolor':
             if len(arg) != 6:
@@ -76,17 +80,22 @@ def main(argv):
                 print usage_text
                 sys.exit(2)
             BALL_COLOR = hex_to_rgb(arg)
+        elif opt == '--radius':
+            RADIUS = clamp(float(arg), 0.01, 5)
         elif opt == '--meter':
-            METER = clamp(float(arg), 50, 3000)
+            METER = clamp(float(arg), 50, 10000)
         elif opt == '--gravity':
-            GRAVITY = clamp(float(arg), max=50)
+            GRAVITY = clamp(float(arg), maximum=50)
         elif opt == '--friction':
-            FRICTION = clamp(float(arg), max=2)
+            FRICTION = clamp(float(arg), maximum=2)
         elif opt == '--restitution':
-            RESTITUTION = clamp(float(arg), max=10)
+            RESTITUTION = clamp(float(arg), maximum=10)
 
     if BG_COLOR == BALL_COLOR:
         print("warning: bgcolor and ballcolor should not be identical!")
+        sys.exit(1)
+    if RADIUS * 2 * METER > min(SIZE):
+        print("warning: ball radius too large for given window size!")
         sys.exit(1)
 
     pygame.init()
@@ -125,8 +134,8 @@ def calculate_speed(velocity):
 
 
 class Ball:
-    def __init__(self, center_location=(WIDTH / 2, HEIGHT / 2), rad=DEFAULT_RADIUS, vel=(0.0, 0.0)):
-        r = int(rad * METER)
+    def __init__(self, center_location=(WIDTH / 2, HEIGHT / 2), vel=(0.0, 0.0)):
+        r = int(RADIUS * METER)
         self.image = pygame.Surface((2 * r, 2 * r))
         self.image.set_colorkey(BG_COLOR)
         self.image.fill(BG_COLOR)
@@ -138,7 +147,6 @@ class Ball:
         pygame.draw.circle(self.shadow, BG_COLOR, (r, r), r, 0)
 
         self.vel = list(vel)
-        self.r = rad
 
         self.held = False
         self.exploded = False
@@ -146,19 +154,20 @@ class Ball:
         self.rect = self.image.get_rect(center=center_location)
         self.old_rect = None
 
-    def move(self, TIME):
+    def move(self, interval):
 
         # apply gravity to ball
-        self.vel[1] += GRAVITY * TIME
+        self.vel[1] += GRAVITY * interval
 
         # TODO when gravity == 0, this crashes program
         # apply air resistance to ball
         speed = calculate_speed(self.vel)
-        self.vel[0] -= 0.1 * TIME * self.vel[0] / speed
-        self.vel[1] -= 0.1 * TIME * self.vel[1] / speed
+        drag_force = AIR_DENSITY * DRAG * math.pi * RADIUS**2 / 2 * speed**2
+        self.vel[0] -= drag_force * interval * self.vel[0] / speed
+        self.vel[1] -= drag_force * interval * self.vel[1] / speed
 
         # update ball location and velocity
-        self.rect.move_ip([x * TIME * METER for x in self.vel])
+        self.rect.move_ip([x * interval * METER for x in self.vel])
         self.rect.clamp_ip(screen.get_rect())
         assert self.rect.left >= 0 and self.rect.top >= 0, "screen dimensions invalid"
         if self.rect.left == 0:
@@ -174,18 +183,19 @@ class Ball:
             # prevents ball from "jittering" when at bottom of screen
             if abs(self.vel[1]) < 0.05:  # TODO remove this magic number somehow
                 self.vel[1] = 0
-                delta = FRICTION * GRAVITY * TIME
-                # TODO when friction is set to 2, there's a weird bug where sometimes it doesn't make it into this block so it just keeps going without friction
+                delta = FRICTION * GRAVITY * interval
+                # TODO when friction is set to 2, there's a weird bug where sometimes it doesn't make it
+                #  into this block so it just keeps going without friction
                 if self.vel[0] > 0:
-                    self.vel[0] = clamp(self.vel[0] - delta, min=0)
+                    self.vel[0] = clamp(self.vel[0] - delta, minimum=0)
                 else:
-                    self.vel[0] = clamp(self.vel[0] + delta, max=0)
+                    self.vel[0] = clamp(self.vel[0] + delta, maximum=0)
             else:
                 self.vel[1] = -abs(self.vel[1])
                 self.bounce_calculation(1)
 
     def drag(self, interval):
-        coords = [x - self.r * METER for x in pygame.mouse.get_pos()]
+        coords = [x - RADIUS * METER for x in pygame.mouse.get_pos()]
         self.rect.left = coords[0]
         self.rect.top = coords[1]
         delta = pygame.mouse.get_rel()
